d04898b91750fe0c83e4fbc7b9e3ba5f038c2152
Raul Garcia <luzontuells@gmail.com>
2016-11-24 18:00:43 +0100

    24/11 - 18:00

--- app/src/main/java/com/example/lab9/MainActivity.java
+++ app/src/main/java/com/example/lab9/MainActivity.java
@@ -1,13 +1,92 @@
 package com.example.lab9;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG_MAIN_ACTIVITY = "In-MainActivity";
+    private static final String MY_SHARED_PREFERENCES = "MySharedPrefsKey";
+    private static final String outputFilename = "internalStorageFile.txt";
+    //private static final String outputFilename = "internalStorageFile2.txt";
+
+    private TextView myTextView;
+    private EditText myEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.myTextView = (TextView) this.findViewById(R.id.text_view);
+        this.myEditText = (EditText) this.findViewById(R.id.text_edit);
+        final Button ok_Btn = (Button) this.findViewById(R.id.ok_but);
+        ok_Btn.setOnClickListener(this);
+        final Button internalStorage_Btn = (Button) this.findViewById(R.id.save_int_but);
+        internalStorage_Btn.setOnClickListener(this);
+        final Button externalStorage_Btn = (Button) this.findViewById(R.id.save_ext_but);
+        externalStorage_Btn.setOnClickListener(this);
+        final Button getFiles_Btn = (Button) this.findViewById(R.id.curr_but);
+        getFiles_Btn.setOnClickListener(this);
+        final Button createDB_Btn = (Button) this.findViewById(R.id.create_db);
+        createDB_Btn.setOnClickListener(this);
+
+        SharedPreferences mSettings = this.getSharedPreferences(MainActivity.MY_SHARED_PREFERENCES,
+                Context.MODE_PRIVATE);
+        String mString = mSettings.getString("mtStringKey","");
+    }
+
+    @Override
+    protected void onStop(){
+//        Si la aplicación se cierra completamente (las
+//        Shared Preferences se guardan en el método onStop() del ciclo de vida de la Activity) y se vuelve a iniciar, el
+//        TextView mostrará el último mensaje con que fue actualizado.
+        SharedPreferences mSettings =this.getSharedPreferences(MainActivity.MY_SHARED_PREFERENCES,
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor mEditor = mSettings.edit();
+        mEditor.putString("myStringKey",this.myTextView.getText().toString());
+        mEditor.apply();
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.ok_but:
+                //TODO: Chekear que el texto esté OK
+                this.myTextView.setText(this.myEditText.getText().toString());
+            break;
+            case R.id.save_int_but:
+                try
+                {
+                    FileOutputStream mFileOutputStream =
+                            this.openFileOutput("internalStorageFile.txt", Context.MODE_APPEND);
+                    String outputString = this.myEditText.getText().toString() + "\n";
+                    mFileOutputStream.write(outputString.getBytes());
+                    mFileOutputStream.close();
+                } catch (IOException e) { e.printStackTrace(); }
+            break;
+            case R.id.save_ext_but:
+
+            break;
+            case R.id.create_db:
+
+            break;
+            case R.id.curr_but:
+
+            break;
+            default:
+                break;
+        }
+
     }
 }
